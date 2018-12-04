d247f087b6fbdc3a60cd07d014aa550bcddab115
Rohit Joshi <link2j.roy@gmail.com>
2018-12-04 14:37:32 +0000

    Add config cmd and config check

--- run.sh
+++ run.sh
@@ -10,10 +10,12 @@ function cf-tools() {
           help ;;
         "install-tools" )
           install-tools ;;
+        "config" )
+          config-ok && config ;;
         "create-service" )
-          create-service "$2" ;;
+          config-ok && create-service "$2" ;;
         "create-cluster" )
-          create-cluster "$2" ;;
+          config-ok && create-cluster "$2" ;;
       esac
     else
       echo "Command not recognized."
@@ -38,6 +40,12 @@ function help() {
 function install-tools() {
   which -s aws || pip install awscli
 	which -s jq || ( which -s brew && brew install jq || which -s apt-get && apt-get install jq || which -s yum && yum install jq || which -s choco && choco install jq)
+}
+
+function config() {
+  echoInfo "AWS_PROFILE: ${AWS_PROFILE}"
+  echoInfo "AWS_REGION: ${AWS_REGION}"
+  echoInfo "ENV_LABEL: ${ENV_LABEL}"
 }
 
 function create-service {
@@ -66,4 +74,31 @@ function create-cluster() {
     else
       echo "Cluster name not supplied."
   fi
+}
+
+# Non-exposed
+function config-ok() {
+  if [[ -z "$AWS_PROFILE" || -z "$AWS_REGION"] || -z "$ENV_LABEL" ]]
+    then
+      echo "One of the required env variables are not set."
+      config
+      return 1
+    else
+      return 0
+  fi
+}
+
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+NC='\033[0m'
+
+function echoInfo() {
+  printf "${Cyan}$1${NC}\n"
 }
