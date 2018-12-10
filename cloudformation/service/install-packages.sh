c8b42d03e5a157498aa1906ff4331482b3536ca4
Rohit Joshi <link2j.roy@gmail.com>
2018-12-10 16:16:29 +0000

    Add service-core and correlation.yaml

--- cloudformation/service/install-packages.sh
+++ cloudformation/service/install-packages.sh
@@ -3,16 +3,19 @@ echo "cloning granitic"
 git clone -b dev-1.3.0 --single-branch https://github.com/graniticio/granitic $GOPATH/src/github.com/graniticio/granitic
 echo "fetching granitic"
 go install github.com/graniticio/granitic
+echo "fetching service-core"
+git clone https://$GIT_USER:$GIT_OUTH_TOKEN@github.com/cloudfactory/service-core $GOPATH/src/github.com/cloudfactory/service-core
+
+echo "fetching granitic yaml"
+go get github.com/graniticio/granitic-yaml
+echo "installing granitic grnc-yaml-bind"
+go install github.com/graniticio/granitic-yaml/cmd/grnc-yaml-bind
+echo "installing granitic grnc-ctl"
+go install github.com/graniticio/granitic/cmd/grnc-ctl
 
 echo "fetching aws"
 go get github.com/aws/aws-sdk-go/aws
 echo "fetching satori"
 go get github.com/satori/go.uuid
-echo "fetching granitic yaml"
-go get github.com/graniticio/granitic-yaml
-echo "installing granitic grnc-yaml-bind"
-go install github.com/graniticio/granitic-yaml/cmd/grnc-yaml-bind
-echo "installing granitic grnc-yaml-project"
-go install github.com/graniticio/granitic-yaml/cmd/grnc-yaml-project
-echo "installing granitic grnc-ctl"
-go install github.com/graniticio/granitic/cmd/grnc-ctl
+echo "installing jwt"
+go get github.com/gbrlsnchs/jwt
