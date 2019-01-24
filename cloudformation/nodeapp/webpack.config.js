da9c8c23f7c402c283c64a496b6eced50c746ad6
Gaurav KC <13gaurab@gmail.com>
2019-01-24 14:40:41 +0545

    Add type check and lint commands

--- cloudformation/nodeapp/webpack.config.js
+++ cloudformation/nodeapp/webpack.config.js
@@ -4,29 +4,29 @@ const path = require('path'),
 
 module.exports = {
   entry: {
-      app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
-      vendor: ['react', 'react-dom']
+    app: ['./src/app/App.tsx', 'webpack-hot-middleware/client'],
+    vendor: ['react', 'react-dom']
   },
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      filename: 'js/[name].bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'js/[name].bundle.js'
   },
   devtool: 'source-map',
   resolve: {
-      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
   },
   module: {
-      rules: [
-          {
-              test: /\.(ts|tsx)$/,
-              loader: 'babel-loader'
-          },
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'babel-loader'
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
   },
   plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'app', 'index.html') }),
-      new webpack.HotModuleReplacementPlugin()
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'app', 'index.html') }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
