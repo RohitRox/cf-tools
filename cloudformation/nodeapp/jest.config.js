dc870d7ab93e89abac216e9ecb1342601a9f09c1
acesmndr <acesmndr@gmail.com>
2019-01-24 17:27:32 +0545

    added transform for node modules tsx files

--- cloudformation/nodeapp/jest.config.js
+++ cloudformation/nodeapp/jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  "transformIgnorePatterns": [
+    // "/node_modules/(?!cloudfactory-ui-foundation).+\\.jsx$" Enable to use tsx from node modules
+  ],
   testMatch: [
     "**/__tests__/*.(ts|tsx|js)"
   ]
